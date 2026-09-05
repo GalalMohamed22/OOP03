31c1c88e5952b1f009e4a585eb2963a5135aa69a
GalalMohamed22 <galal9678@gmail.com>
2026-09-05 06:17:36 +0300

    Question01

--- Assignment03OOP/Program.cs
+++ Assignment03OOP/Program.cs
@@ -6,7 +6,21 @@ namespace Assignment03OOP
     {
         static void Main(string[] args)
         {
-          
+
+            #region Question01
+            // a)  What is the difference between Method Overloading and Method Overriding?
+            // Method Overloading occurs when multiple methods in the same class share the same name but have different parameters different in  type, number, or order
+            // Method Overriding occurs when a subclass provides a specific implementation of a method that is already defined in its parent class
+
+
+            // b)  What is the difference between Static Binding and Dynamic Binding? 
+            // Static Binding occurs when the code to be executed is linked to the method call at compile time
+            // Dynamic Binding occurs when the code to be executed is linked to the method call at runtime
+
+            #endregion
+
+
+
 
 
 
